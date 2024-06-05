442779fd8fa46a0dcfc49a2c299c8c3c3c173ef2
Akayy <techakayy@gmail.com>
2024-06-05 20:20:19 +1000

    Using mergeConfig

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import { mergeConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,8 +13,9 @@ export default defineConfig({
     {
       name: 'live-designer',
       config: (config, { command, mode }) => {
-        console.log('testing plain vue vite project')
-        return config
+        return mergeConfig(config, {
+          clearScreen: false
+        })
       }
     }
   ],
